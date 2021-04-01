8aac0f374b38959351fae793c241f8429f0fd8b4
JulyB-creations <79759706+JulyB-creations@users.noreply.github.com>
2021-04-01 19:02:59 +0200

    Integrated Axios

---
+++ src/Weather.js
@@ -0,0 +1,15 @@
+import React from "react";
+import axios from "axios";
+
+export default function Weather(props) {
+  function handleResponse(response) {
+    alert(
+      `The weather in ${response.data.name} is ${response.data.main.temp} degrees.`
+    );
+  }
+  let apiKey = "d4b17c5b0c006239da987428a9e9effa";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=${apiKey}&units=metric`;
+
+  axios.get(apiUrl).then(handleResponse);
+  return <h2>Hello from weather</h2>;
+}
